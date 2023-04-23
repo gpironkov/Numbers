1c60787abe15cab2dc6230d71a4f9c1f4d883c36
g_pironkov <g_pironkov@yahoo.com>
2023-04-24 01:12:41 +0300

    Add project files.

---
+++ Numbers/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Numbers.Models;
+using System;
+using System.Linq;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        private const string NumbersKey = "Numbers";
+        private const string CountKey = "Count";
+        private const string SumKey = "Sum";
+
+        public HomeController(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new NumberModel();
+
+            viewModel.NumbersList = GetNumbersToList();
+            viewModel.Counter = _contextAccessor.HttpContext.Session.GetInt32(CountKey) ?? 0;
+            viewModel.Sum = _contextAccessor.HttpContext.Session.GetInt32(SumKey) ?? 0;
+
+            return View(viewModel);
+        }
+
+        public IActionResult AddNumber()
+        {
+            var random = new Random();
+            var number = random.Next(1, 1001);
+
+            var numbersList = GetNumbersToList();
+            numbersList.Add(number);
+            var numbers = JsonConvert.SerializeObject(numbersList);
+
+            HttpContext.Session.SetString(NumbersKey, numbers);
+
+            var count = HttpContext.Session.GetInt32(CountKey) ?? 0;
+            _contextAccessor.HttpContext.Session.SetInt32(CountKey, ++count);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult SumNumbers()
+        {
+            var numbersList = GetNumbersToList();
+            var sum = numbersList.Sum();
+
+            _contextAccessor.HttpContext.Session.SetInt32(SumKey, sum);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ClearNumbers()
+        {
+            HttpContext.Session.Remove(NumbersKey);
+            HttpContext.Session.Remove(CountKey);
+            HttpContext.Session.Remove(SumKey);
+
+            return RedirectToAction("Index");
+        }
+
+        private List<int> GetNumbersToList()
+        {
+            var numbersJson = HttpContext.Session.GetString(NumbersKey);
+            var numbersList = numbersJson != null ? JsonConvert.DeserializeObject<List<int>>(numbersJson) : new List<int>();
+
+            return numbersList;
+        }
+    }
+}
---
+++ Numbers/Models/NumberModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Numbers.Models
+{
+    public class NumberModel
+    {
+        public List<int>? NumbersList { get; set; }
+
+        public int Counter { get; set; }
+
+        public int Sum { get; set; }
+    }
+}
